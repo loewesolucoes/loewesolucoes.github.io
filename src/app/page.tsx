313e1d1a6cb232e46cd66928f3bcd2cb1f3a246d
Erico de Souza Loewe <erico.loewe@dbccompany.com.br>
2024-04-16 11:07:29 -0300

    about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,35 +42,19 @@ function Home() {
         <div className="welcome">
           <section className="welcome-info">
             <h1>Inovação e tecnologia ao seu alcance</h1>
+            <hr />
             <p>Com uma abordagem centrada no cliente, oferecemos ferramentas personalizadas que otimizam processos, aumentam a eficiência e impulsionam o crescimento sustentável</p>
             <button type="button" className="btn btn-sm btn-info">Conheça nossos serviços</button>
           </section>
         </div>
         <div className="about">
           <section>
-            <h3>Sobre nosso trabalho</h3>
+            <h2>Sobre nosso trabalho</h2>
+            <hr />
             <p>Na Loewe Soluções em Software, nós nos dedicamos a desenvolver aplicações inovadoras para a gestão de negócios. Nossa equipe é composta por profissionais altamente qualificados e apaixonados por tecnologia, comprometidos em entregar soluções que transformam a maneira como as empresas operam. Com uma abordagem centrada no cliente, oferecemos ferramentas personalizadas que otimizam processos, aumentam a eficiência e impulsionam o crescimento sustentável. Junte-se a nós e leve seu negócio ao próximo nível com inteligência e tecnologia.</p>
           </section>
         </div>
       </article>
-      {/* <div className="main d-flex">
-        <article className="d-flex flex-column my-5 container">
-          <section className="welcome">
-            <h1>Inovação e tecnologia ao seu alcance</h1>
-            <p>Com uma abordagem centrada no cliente, oferecemos ferramentas personalizadas que otimizam processos, aumentam a eficiência e impulsionam o crescimento sustentável</p>
-            <button type="button" className="btn btn-sm btn-info">Conheça nossos serviços</button>
-          </section>
-        </article>
-        <img className="bg" src="bg-full.png" />
-      </div>
-      <div className="about d-flex">
-        <article className="d-flex my-5 container justify-content-center">
-          <section>
-            <h3>Sobre nosso trabalho</h3>
-            <p>Na Loewe Soluções em Software, nós nos dedicamos a desenvolver aplicações inovadoras para a gestão de negócios. Nossa equipe é composta por profissionais altamente qualificados e apaixonados por tecnologia, comprometidos em entregar soluções que transformam a maneira como as empresas operam. Com uma abordagem centrada no cliente, oferecemos ferramentas personalizadas que otimizam processos, aumentam a eficiência e impulsionam o crescimento sustentável. Junte-se a nós e leve seu negócio ao próximo nível com inteligência e tecnologia.</p>
-          </section>
-        </article>
-      </div> */}
     </>
   );
 
